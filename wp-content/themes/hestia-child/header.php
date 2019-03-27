724e9db8770a936ac59d0de8ca1413761bb9a2a6
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-27 15:34:29 +0200

    change newsletter form

--- wp-content/themes/hestia-child/header.php
+++ wp-content/themes/hestia-child/header.php
@@ -65,11 +65,29 @@
                 <img src="<?php echo get_template_directory_uri(); ?>-child/assets/images/pictograms.svg" alt="Pictorgams" itemprop="contentUrl"/>
             </figure>
             <div class="mailchimp-form">
-                <div class="email-octopus-form-wrapper">
+                <div id="mc_embed_signup">
+                    <div class="title">SUBSCRIBE TO OUR LIST</div>
+                    <form action="https://dentacoin.us16.list-manage.com/subscribe/post?u=61ace7d2b009198ca373cb213&amp;id=993df5967d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                        <div id="mc_embed_signup_scroll">
+                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                            <div class="fs-0 checkbox-container">
+                                <div class="inline-block-top checkbox-wrapper">
+                                    <input type="checkbox" id="consent" name="consent" required>
+                                </div>
+                                <label for="consent" class="privacy-policy-text inline-block-top"><span> As per your request, we will use the data provided to send you newsletters. You can change your mind any time by clicking the unsubscribe link in the footer of any marketing email your receive from us. By clicking on the SIGN UP button, you agree to our <a href="//dentacoin.com/privacy-policy">Privacy Policy</a></span></label>
+                            </div>
+                            <div class="form-row btn-container">
+                                <input type="hidden" name="b_61ace7d2b009198ca373cb213_993df5967d" tabindex="-1" value="">
+                                <div class="clear btn-container"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!--<div class="email-octopus-form-wrapper">
                     <p class="email-octopus-success-message"></p>
                     <p class="email-octopus-error-message"></p>
                     <form method="post" action="https://emailoctopus.com/lists/398a309b-ef00-11e8-a3c9-06b79b628af2/members/embedded/1.3s/add" class="email-octopus-form" data-sitekey="6LdYsmsUAAAAAPXVTt-ovRsPIJ_IVhvYBBhGvRV6" data-success-message="Thank you for signing up!">
-                        <div class="title"> SUBSCRIBE TO OUR LIST</div>
+                        <div class="title">SUBSCRIBE TO OUR LIST</div>
                         <div class="form-row fs-0 flex email-octopus-form-row" data-valid-email-message="Please enter valid email.">
                             <input id="field_0" name="field_0" type="email" placeholder="Email address">
                             <input type="hidden" name="successRedirectUrl" value="">
@@ -84,11 +102,10 @@
                             <button type="submit">SIGN UP</button>
                         </div>
                         <div class="email-octopus-form-row-hp" aria-hidden="true">
-                            <!-- Do not remove this field, otherwise you risk bot sign-ups -->
                             <input type="text" name="hp398a309b-ef00-11e8-a3c9-06b79b628af2" tabindex="-1" autocomplete="nope"/>
                         </div>
                     </form>
-                </div>
+                </div>-->
             </div>
             <div class="apps-nav">
                 <ul itemscope="" itemtype="http://schema.org/SiteNavigationElement">
