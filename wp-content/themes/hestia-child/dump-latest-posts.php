b71409ace5c267a8765d5a9f01c549161f63257e
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-13 14:22:41 +0300

    test dump latest posts

--- wp-content/themes/hestia-child/dump-latest-posts.php
+++ wp-content/themes/hestia-child/dump-latest-posts.php
@@ -9,20 +9,20 @@ $the_query = new WP_Query(array(
 
 ));
 
-$arr = '';
-$arr_end = '';
-$num = 0;
+$array = array();
 if($the_query->have_posts()):
     while($the_query->have_posts()):$the_query->the_post();
-        $num+=1;
         $post_data = get_post_meta($post->ID);
-        if($num < $the_query->post_count) {
-            $single_arr_end = ',';
-        }else {
-            $single_arr_end = '';
-        }
-        $arr.='[{"post_title":"'.$post->post_title.'"},{"post_content":"'.substr(str_replace('"', "'", strip_tags($post->post_content)), 0, 50).'"},{"thumb":"'.get_the_post_thumbnail_url($post->ID).'"},{"link":"'.get_permalink($post->ID).'"},{"date":'.strtotime($post->post_date).'}]'.$single_arr_end;
+        $current_post_arr = array(
+            "post_title" => $post->post_title,
+            "post_content" => substr(str_replace('"', "'", strip_tags($post->post_content)), 0, 50),
+            "thumb" => get_the_post_thumbnail_url($post->ID),
+            "link" => get_permalink($post->ID),
+            "date" => strtotime($post->post_date)
+        );
+
+        array_push($array, $current_post_arr);
     endwhile;
 endif;
-echo $arr.$arr_end;
+echo json_encode($array);
 die();
