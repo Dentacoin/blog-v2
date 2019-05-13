5ca8ec4070ca79cc44b7855bf4e40768f815fad6
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-13 13:26:24 +0300

    dump latest post data

--- wp-content/themes/hestia-child/dump-latest-posts.php
+++ wp-content/themes/hestia-child/dump-latest-posts.php
@@ -14,7 +14,6 @@ $arr_end = ']';
 $num = 0;
 if($the_query->have_posts()):
     while($the_query->have_posts()):$the_query->the_post();
-        var_dump($post);
         $num+=1;
         $post_data = get_post_meta($post->ID);
         if($num < $the_query->post_count) {
@@ -22,8 +21,8 @@ if($the_query->have_posts()):
         }else {
             $single_arr_end = '';
         }
-        $arr.='[{"post_title":"'.$post->post_title.'"},{"post_content":"'.substr($post->post_content, 0, 50).'"},{"thumb":"'.get_the_post_thumbnail_url($post->ID).'"},{"link":"'.get_permalink($post->ID).'"},{"date":"'.get_permalink($post->ID).'"}]'.$single_arr_end;
+        $arr.='[{"post_title":"'.$post->post_title.'"},{"post_content":"'.substr($post->post_content, 0, 50).'"},{"thumb":"'.get_the_post_thumbnail_url($post->ID).'"},{"link":"'.get_permalink($post->ID).'"},{"date":"'.get_permalink($post->post_date).'"}]'.$single_arr_end;
     endwhile;
 endif;
-//echo $arr.$arr_end;
+echo $arr.$arr_end;
 die();
