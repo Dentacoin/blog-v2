0e2d88460960eed8f3b5f626749f8ab588827929
unknown <miroslav.nedelchev@dentacoin.com>
2020-05-26 16:24:35 +0300

    add defer image load for the blog

--- wp-content/themes/hestia-child/includes/styles-and-scripts.php
+++ wp-content/themes/hestia-child/includes/styles-and-scripts.php
@@ -9,10 +9,10 @@ function dcn_add_styles_and_scripts() {
 
     wp_enqueue_script('emailoctopus-script', get_template_directory_uri() . '-child/assets/libs/emailoctopus/emailoctopus.js', array( 'jquery' ) );
 
-    wp_enqueue_style('custom_style', get_template_directory_uri() . '-child/assets/css/style.css', array(), '1.0.8', 'all' );
+    wp_enqueue_style('custom_style', get_template_directory_uri() . '-child/assets/css/style.css', array(), '1.0.9', 'all' );
 
-    wp_enqueue_script('basic_custom_script', 'https://dentacoin.com/assets/js/basic.js', array(), '1.0.8', true);
+    wp_enqueue_script('basic_custom_script', get_template_directory_uri() . '/assets/js/basic.js', array(), '1.0.9', true);
 
-    wp_enqueue_script('custom_script', get_template_directory_uri() . '-child/assets/js/index.js', array(), '1.0.8', true);
+    wp_enqueue_script('custom_script', get_template_directory_uri() . '-child/assets/js/index.js', array(), '1.0.9', true);
 }
 add_action('wp_enqueue_scripts', 'dcn_add_styles_and_scripts');
