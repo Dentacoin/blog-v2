af833d2a0d41aeaecaa33c0d964d6cb9c2870631
unknown <miroslav.nedelchev@dentacoin.com>
2020-05-26 16:28:12 +0300

    dump all media

--- wp-content/themes/hestia-child/404.php
+++ wp-content/themes/hestia-child/404.php
@@ -1,5 +1,19 @@
 <?php
 get_header();
+$query_images_args = array(
+    'post_type'      => 'attachment',
+    'post_mime_type' => 'image',
+    'post_status'    => 'inherit',
+    'posts_per_page' => - 1,
+);
+
+$query_images = new WP_Query( $query_images_args );
+var_dump($query_images->posts);
+
+/*$images = array();
+foreach ( $query_images->posts as $image ) {
+    $images[] = wp_get_attachment_url( $image->ID );
+}*/
 ?>
     <div class="container-404">
         <figure class="logo" itemscope="" itemtype="http://schema.org/ImageObject">
