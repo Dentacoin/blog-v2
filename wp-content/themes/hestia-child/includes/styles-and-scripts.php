bc5f5dd564e7ff0b439eddcae325f5051f6ee277
unknown <miroslav.nedelchev@dentacoin.com>
2020-08-17 17:43:24 +0300

    replace cookie

--- wp-content/themes/hestia-child/includes/styles-and-scripts.php
+++ wp-content/themes/hestia-child/includes/styles-and-scripts.php
@@ -14,5 +14,11 @@ function dcn_add_styles_and_scripts() {
     wp_enqueue_script('basic_custom_script', get_template_directory_uri() . '-child/assets/js/basic.js', array(), '1.0.9', true);
 
     wp_enqueue_script('custom_script', get_template_directory_uri() . '-child/assets/js/index.js', array(), '1.0.9', true);
+
+    if(empty($_COOKIE['performance_cookies']) && empty($_COOKIE['functionality_cookies']) && empty($_COOKIE['marketing_cookies']) && empty($_COOKIE['strictly_necessary_policy'])) {
+        wp_enqueue_style('combined_cookie_style', 'https://dentacoin.com/assets/libs/dentacoin-package/css/style-cookie.css', array(), time(), 'all' );
+
+        wp_enqueue_script('combined_cookie_script', 'https://dentacoin.com/assets/libs/dentacoin-package/js/init.js', array(), time(), true);
+    }
 }
 add_action('wp_enqueue_scripts', 'dcn_add_styles_and_scripts');
